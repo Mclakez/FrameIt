4fda01030292937fe452ab340717c2493f3b9b74
Mclakez <fadairomarvellous@gmail.com>
2026-06-10 20:17:47 +0100

    Added sameSite of the cookie to none

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -19,6 +19,7 @@ async (req, res) => {
             res.cookie("token", token, {
                 httpOnly: true,
                 secure: true,
+                sameSite: 'none',
                 maxAge: 7*24*60*60*1000
             })
              res.redirect(`${process.env.CLIENT_URL}/auth-success`)
